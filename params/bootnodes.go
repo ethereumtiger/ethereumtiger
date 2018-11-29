e3e8b0f8baadfcface116fe953ef4da95923a995
ethereumtiger <ethereumtiger@gmail.com>
2018-11-29 22:29:53 +0300

    new bootnode

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -29,7 +29,7 @@ var MainnetBootnodes = []string{
 	// Ethereum Foundation C++ Bootnodes
 	"enode://979b7fa28feeb35a4741660a16076f1943202cb72b6af70d327f053e248bab9ba81760f39d0701ef1d8f89cc1fbd2cacba0710a12cd5314d5e0c9021aa3637f9@5.1.83.226:30303", // DE*/
 
-	"enode://0936a32adb4d552e6a0c424619cbc636a1c24c382f1ecdbd142ad4957b8a029ceecc9eb97fd6969bb89988cb75df26722a298512143385ba15966e7ae4c919af@3.16.169.223:30707", // DE
+	"enode://c74f3d77b278e4803ed49f4dc34a8b7cf04383b8fe50266cb63cb81413fb23685583561e1d8b21541d0ef7bdb3c132ff829dc19cdc86b3f58a75f6516ccf12ff@3.16.169.223:30707", // DE
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
